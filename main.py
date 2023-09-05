3d92b1ed2b14280bb5b3544357bae13e3160f54a
shibbir <shibbir63@yahoo.com>
2023-09-05 16:12:01 +0100

    Add total, company name and logo.

--- main.py
+++ main.py
@@ -3,34 +3,46 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
+# Extracting file paths in a list from invoices folder
 filepaths = glob.glob("invoices/*.xlsx")
 # print(filepaths)
 # pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+# Creating separate pdf files for each invoice
 for filepath in filepaths:
 
+    # Creating a pdf instance and adding page
     pdf = FPDF(orientation="L", unit="mm", format="A4")
     pdf.add_page()
 
+    # Extracting filename portion from the file path
     filename = Path(filepath).stem
     print(filename)
 
+    # Unpacking data/variables from the filename
     # invoice_nr = filename.split("-")[0]
     invoice_nr, date = filename.split("-")
     print(invoice_nr)
 
+    # Adding a pdf cell for showing invoice number
     pdf.set_font(family="Times", style="B", size=14)
     pdf.cell(h=10, w=0, txt=f"Invoice_no: {invoice_nr}",  ln=1)
 
     # date = filename.split("-")[1]
-    # pdf.line()
+
+    # Adding a pdf cell for showing the date
     pdf.cell(h=10, w=0, txt=f"Date: {date}", ln=3)
 
+    # Reading data from Excel file to create a dataframe
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     print(df)
+
+    # Replace underscores from column header with first letters capitalised
     cols = list(df.columns)
     cols = [col.replace("_", " ").title() for col in cols]
     print(cols)
 
+    # Add column title
     pdf.set_font(family="Times", style="B", size=14)
     pdf.cell(w=30, h=8, txt=cols[0], border=1)
     pdf.cell(w=70, h=8, txt=cols[1], border=1)
@@ -38,6 +50,7 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=cols[3],border=1)
     pdf.cell(w=50, h=8, txt=cols[4],border=1,ln=1)
 
+    # Populate table with data
     for index, row in df.iterrows():
         pdf.set_font(family="Times", style="", size=14)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
@@ -46,52 +59,25 @@ for filepath in filepaths:
         pdf.cell(w=50, h=8, txt=str(row["price_per_unit"]),border=1)
         pdf.cell(w=50, h=8, txt=str(row["total_price"]),border=1,ln=1)
 
+    # Calculate total price to be shown in the table
+    sum_price = sum(df["total_price"])
+
+    # Add rows to the table
     pdf.set_font(family="Times", style="B", size=14)
     pdf.cell(w=30, h=8, txt="Total", border=1)
     pdf.cell(w=70, h=8, txt="", border=1)
     pdf.cell(w=50, h=8, txt="", border=1)
     pdf.cell(w=50, h=8, txt="", border=1)
-    pdf.cell(w=50, h=8, txt="", border=1, ln=1)
+    pdf.cell(w=50, h=8, txt=f"{sum_price}", border=1, ln=1)  # str(sum_price)
 
+    # Add a sentence showing total price.
     pdf.set_font(family="Times", style="B", size=14)
-    pdf.cell(w=0, h=8, txt="The total due amount is:", ln=1)
+    pdf.cell(w=0, h=8, txt=f"The total due amount is: {sum_price}", ln=1)
 
-
-
-
-
-    # pdf.text(x=10, y=10, txt="The total due amount is: ")
-
-
-
-
-
-
+    # Add log and image
+    pdf.set_font(family="Times", style="B", size=20)
+    pdf.cell(w=47, h=8, txt=f"Innovative Co.")
+    pdf.image("image.jpg", w=15)
 
     pdf.output(f"pdf/{filename}.pdf")
 
-
-
-
-
-# # df = pd.read_excel("invoices/10001-2023.1.18.xlsx")
-#
-# print(df)
-#
-# pdf = FPDF(orientation="P", format="A4")
-#
-# pdf.add_page()
-# # Set the header
-# pdf.set_font(family="Times", style="B", size=24) # this line is a must before adding cell type.Without it, 'unifontsubset' error shows up
-# # pdf.set_text_color(100, 100, 100)
-# pdf.cell(w=0, h=12, txt="Topic", align="L", ln=1)
-#
-
-#
-#
-#
-#
-# for item in df.iterrows():
-#     print(item)
-#     for i in range(len(item)):
-#         print(item[1][i])
